d6440180f73fb588499c1a1496ece89d3e6eb0d7
Fredy <fredy-alexander1@hotmail.com>
2026-09-03 17:12:26 -0700

    actualizacion imagenes de barberia

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ const projectsData = {
             "assets/img/barberiaprofile.png",
             "assets/img/barberiaadmin.png",
             "assets/img/barberiaservices.png",
-            "assets/img/barberiaserviceadmin.png"
+            "assets/img/barberserviceadmin.png"
         ],
         live: "https://fredyalvarezz.github.io/barberia/",
         github: "https://github.com/fredyalvarezz/barberia"
